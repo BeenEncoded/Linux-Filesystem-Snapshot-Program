713c7edb0e9a296df9839b90562f67b51708b1b8
Jonathan <jonathancw@me.com>
2014-10-01 15:34:03 -0700

    added time_class (UNTESTED);
    Signed-off-by: Jonathan <jonathancw@me.com>

--- source/Classes/Mediums/time_class.cpp
+++ source/Classes/Mediums/time_class.cpp
@@ -1,6 +1,7 @@
 #include <ctime>
 #include <chrono>
 #include <iostream>
+#include <cstring>
 
 #include "time_class.hpp"
 #include "global_defines.hpp"
@@ -8,6 +9,39 @@
 
 namespace
 {
+    std::vector<unsigned short> month_days(const unsigned int&);
+    unsigned int total_days_in_m(const int&, const int&);
+    
+    
+    inline std::vector<unsigned short> month_days(const unsigned int& year)
+    {
+        return std::vector<unsigned short>({
+            31,
+                    (__isleap(year) ? (unsigned short)29 : (unsigned short)28),
+                    31,
+                    30,
+                    31,
+                    30,
+                    31,
+                    31,
+                    30,
+                    31,
+                    30,
+                    31});
+    }
+    
+    inline unsigned int total_days_in_m(const int& m, const int& year)
+    {
+        unsigned int days(0);
+        
+        for(int x = 0, month = 0; x < (m + 1); x++, (month = ((month + 1) % 12)))
+        {
+            days += month_days(year)[month];
+        }
+        return days;
+    }
+    
+    
 }
 
 /** Stream operators: */
@@ -56,6 +90,271 @@ namespace tdata
 /** Time_class member functions: */
 namespace tdata
 {
+    time_class::time_class() : cur_time(current_time())
+    {
+    }
+    
+    time_class::~time_class()
+    {
+    }
+    
+    const struct tm& time_class::value() const
+    {
+        return this->cur_time;
+    }
+    
+    const time_class& time_class::operator=(const time_class& t)
+    {
+        if(this != &t)
+        {
+            std::memcpy(&(this->cur_time), &t, sizeof(t));
+        }
+        return *this;
+    }
+    
+    const time_class& time_class::operator=(const struct tm& t)
+    {
+        if(&(this->cur_time) != (&t))
+        {
+            std::memcpy(&(this->cur_time), &t, sizeof(t));
+        }
+        return *this;
+    }
+    
+    bool time_class::operator==(const time_class& t) const
+    {
+        return ((this->cur_time.tm_sec == t.cur_time.tm_sec) && 
+                    (this->cur_time.tm_min == t.cur_time.tm_min) && 
+                    (this->cur_time.tm_hour == t.cur_time.tm_hour) && 
+                    (this->cur_time.tm_mday == t.cur_time.tm_mday) && 
+                    (this->cur_time.tm_wday == t.cur_time.tm_wday) && 
+                    (this->cur_time.tm_yday == t.cur_time.tm_yday) && 
+                    (this->cur_time.tm_mon == t.cur_time.tm_mon) && 
+                    (this->cur_time.tm_year == t.cur_time.tm_year));
+    }
+    
+    bool time_class::operator!=(const time_class& t) const
+    {
+        return ((this->cur_time.tm_sec != t.cur_time.tm_sec) && 
+                    (this->cur_time.tm_min != t.cur_time.tm_min) && 
+                    (this->cur_time.tm_hour != t.cur_time.tm_hour) && 
+                    (this->cur_time.tm_mday != t.cur_time.tm_mday) && 
+                    (this->cur_time.tm_wday != t.cur_time.tm_wday) && 
+                    (this->cur_time.tm_yday != t.cur_time.tm_yday) && 
+                    (this->cur_time.tm_mon != t.cur_time.tm_mon) && 
+                    (this->cur_time.tm_year != t.cur_time.tm_year));
+    }
+    
+    bool time_class::operator<(const time_class& t) const
+    {
+        if(this->cur_time.tm_year < t.cur_time.tm_year) return true;
+        else if(this->cur_time.tm_year == t.cur_time.tm_year)
+        {
+            if(this->cur_time.tm_yday < t.cur_time.tm_yday) return true;
+            else if(this->cur_time.tm_yday == t.cur_time.tm_yday)
+            {
+                if(this->cur_time.tm_hour < t.cur_time.tm_hour) return true;
+                else if(this->cur_time.tm_hour == t.cur_time.tm_hour)
+                {
+                    if(this->cur_time.tm_min < t.cur_time.tm_min) return true;
+                    else if(this->cur_time.tm_min == t.cur_time.tm_min)
+                    {
+                        if(this->cur_time.tm_sec < t.cur_time.tm_sec) return true;
+                    }
+                }
+            }
+        }
+        return false;
+    }
+    
+    bool time_class::operator<=(const time_class& t) const
+    {
+        return (this->operator<(t) || this->operator==(t));
+    }
+    
+    bool time_class::operator>(const time_class& t) const
+    {
+        return (this->operator!=(t) && !(this->operator<(t)));
+    }
+    
+    bool time_class::operator>=(const time_class& t) const
+    {
+        return !(this->operator<(t));
+    }
+    
+    const time_class& time_class::operator++(int t __attribute__((unused)))
+    {
+        this->cur_time.tm_sec++;
+        if(this->cur_time.tm_sec > 59)
+        {
+            this->cur_time.tm_sec %= 60;
+            this->cur_time.tm_min++;
+            if(this->cur_time.tm_min > 59)
+            {
+                this->cur_time.tm_min %= 60;
+                this->cur_time.tm_hour++;
+                if(this->cur_time.tm_hour > 23)
+                {
+                    this->cur_time.tm_hour %= 24;
+                    this->cur_time.tm_yday++;
+                    this->cur_time.tm_wday++;
+                    this->cur_time.tm_wday %= 7;
+                    if(this->cur_time.tm_yday > (__isleap(this->cur_time.tm_year) ? 364 : 365))
+                    {
+                        this->cur_time.tm_year++;
+                        this->cur_time.tm_yday = 0;
+                    }
+                    this->sync_to_yday();
+                }
+            }
+        }
+        return *this;
+    }
+    
+    time_class time_class::operator+(int i) const
+    {
+        time_class tempt(*this);
+        if(i < 0) for(int x = 0; x < (i * (-1)); x++) tempt--;
+        else if(i > 0) for(int x = 0; x < i; x++) tempt++;
+        return tempt;
+    }
+    
+    const time_class& time_class::operator+=(int i)
+    {
+        (*this) = ((*this) + i);
+        return *this;
+    }
+    
+    const time_class& time_class::operator--(int t __attribute__((unused)))
+    {
+        if(this->cur_time.tm_sec == 0)
+        {
+            this->cur_time.tm_sec = 59;
+            if(this->cur_time.tm_min == 0)
+            {
+                this->cur_time.tm_min = 59;
+                if(this->cur_time.tm_hour == 0)
+                {
+                    this->cur_time.tm_hour = 23;
+                    if(this->cur_time.tm_yday == 0)
+                    {
+                        if(this->cur_time.tm_year > 0)
+                        {
+                            this->cur_time.tm_year--;
+                        }
+                        
+                        if(this->cur_time.tm_yday == 0) this->cur_time.tm_yday = (__isleap(this->cur_time.tm_year) ? 364 : 365);
+                        else if(this->cur_time.tm_yday > 0) this->cur_time.tm_yday--;
+                        
+                        if(this->cur_time.tm_wday == 0) this->cur_time.tm_wday = 6;
+                        else if(this->cur_time.tm_wday > 0) this->cur_time.tm_wday--;
+                    }
+                    else if(this->cur_time.tm_yday > 0) this->cur_time.tm_yday--;
+                }
+                else if(this->cur_time.tm_hour > 0) this->cur_time.tm_hour--;
+            }
+            else if(this->cur_time.tm_min > 0) this->cur_time.tm_min--;
+        }
+        else if(this->cur_time.tm_sec > 0) this->cur_time.tm_sec--;
+        this->sync_to_yday();
+        return *this;
+    }
+    
+    time_class time_class::operator-(int i) const
+    {
+        time_class tempt(*this);
+        if(i < 0) for(int x = 0; x < (i * (-1)); x++) tempt++;
+        else if(i > 0) for(int x = 0; x < i; x++) tempt--;
+        return tempt;
+    }
+    
+    const time_class& time_class::operator-=(int i)
+    {
+        (*this) = ((*this) - i);
+        return *this;
+    }
+    
+    int& time_class::second()
+    {
+        return this->cur_time.tm_sec;
+    }
+    
+    int& time_class::minute()
+    {
+        return this->cur_time.tm_min;
+    }
+    
+    /* Hour in military format */
+    int& time_class::mhour()
+    {
+        return this->cur_time.tm_hour;
+    }
+    
+    int& time_class::month()
+    {
+        return this->cur_time.tm_mon;
+    }
+    
+    int& time_class::wday()
+    {
+        return this->cur_time.tm_wday;
+    }
+    
+    /* Hour in 12-hour format */
+    int time_class::hour() const
+    {
+        int tempi(this->cur_time.tm_hour % 12);
+        if(tempi == 0) tempi = 12;
+        return tempi;
+    }
+    
+    bool time_class::am() const
+    {
+        return (this->cur_time.tm_hour < 12);
+    }
+    
+    int time_class::gyear() const
+    {
+        return (this->cur_time.tm_year + 1900);
+    }
+    
+    void time_class::syear(const int& i)
+    {
+        unsigned int tempyday(this->cur_time.tm_yday);
+        int target(i - 1900);
+        
+        if(target < this->cur_time.tm_year)
+        {
+            while(target < this->cur_time.tm_year) (*this) = this->operator-(vday::value);
+        }
+        else if(target > this->cur_time.tm_year)
+        {
+            while(target > this->cur_time.tm_year) (*this) = this->operator+(vday::value);
+        }
+        this->cur_time.tm_yday = tempyday;
+        this->sync_to_yday();
+    }
+    
+    /* Syncs the month and month day to the day of the year.  This allows
+     * us to simply do math with yday, and then "sync" the month and day of 
+     * month to the yday. */
+    void time_class::sync_to_yday()
+    {
+        this->cur_time.tm_mday = 1;
+        for(this->cur_time.tm_mon = 0; 
+                        ((this->cur_time.tm_mon < 12) && ((signed)total_days_in_m(this->cur_time.tm_mon, this->cur_time.tm_year) < this->cur_time.tm_yday)); 
+                        this->cur_time.tm_mon++);
+        if(this->cur_time.tm_yday < month_days(this->cur_time.tm_year).at(this->cur_time.tm_mon))
+        {
+            this->cur_time.tm_mday = (this->cur_time.tm_yday + 1);
+        }
+        else
+        {
+            this->cur_time.tm_mday = (this->cur_time.tm_yday - total_days_in_m((this->cur_time.tm_mon - 1), this->cur_time.tm_year));
+        }
+    }
+    
+    
 }
 
 /** misc functions: */
@@ -63,7 +362,10 @@ namespace tdata
 {
     struct tm current_time()
     {
-        return tptotm(std::chrono::system_clock::now());
+        time_point now(std::chrono::system_clock::now());
+        struct tm temptm;
+        tptotm(now, temptm);
+        return temptm;
     }
     
     
--- source/Classes/Mediums/time_class.hpp
+++ source/Classes/Mediums/time_class.hpp
@@ -1,14 +1,16 @@
 #ifndef TIME_CLASS_HPP_INCLUDED
 #define TIME_CLASS_HPP_INCLUDED
-#include <chrono>
 #include <ctime>
-#include <ratio>
 #include <iostream>
+
+#include "global_defines.hpp"
 
 namespace tdata
 {
     typedef std::chrono::system_clock clock_type;
     typedef std::chrono::time_point<clock_type> time_point;
+    
+    class time_class;
     
     std::ostream& operator<<(std::ostream&, time_point&);
     std::istream& operator>>(std::istream&, time_point&);
@@ -19,14 +21,55 @@ namespace tdata
     class time_class
     {
     public:
+    
+        //time constants: 
+        typedef const_int_type<60> vminute;
+        typedef const_int_type<(vminute::value * 60)> vhour;
+        typedef const_int_type<(vhour::value * 24)> vday;
+    
         explicit time_class();
         ~time_class();
         
-        const time_point& value() const;
+        const struct tm& value() const;
         
+        const time_class& operator=(const time_class&);
+        const time_class& operator=(const struct tm&);
+        
+        //comparisons
+        bool operator==(const time_class&) const;
+        bool operator!=(const time_class&) const;
+        bool operator<(const time_class&) const;
+        bool operator>(const time_class&) const;
+        bool operator<=(const time_class&) const;
+        bool operator>=(const time_class&) const;
+        
+        //mathematical ops: 
+        const time_class& operator++(int);
+        time_class        operator+(int) const;
+        const time_class& operator+=(int);
+        const time_class& operator--(int);
+        time_class        operator-(int) const;
+        const time_class& operator-=(int);
+        
+        /* data retrieval.  I hate to do this, but the information
+         * must be modified before reads and after writes. */
+        
+        int& second();//0-59
+        int& mhour(); //0-23
+        int& minute();//0-59
+        int& month(); //0-11 
+        int& wday();  //0-6
+        int hour() const;
+        bool am() const;
+        int gyear() const;
+        void syear(const int&);
+        std::string month_name();
+        std::string wday_name();
         
     private:
-        time_point cur_time;
+        struct tm cur_time;
+        
+        void sync_to_yday();
         
     };
     
