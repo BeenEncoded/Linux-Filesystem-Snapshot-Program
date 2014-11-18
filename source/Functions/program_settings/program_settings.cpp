08689178d7f80dfbddf1c79a0ba9f3893f1dde4e
Jonathan <jonathancw@me.com>
2014-11-17 16:59:44 -0800

    added operators for regex_data; made settings structures typdefed for efficiency;
    Signed-off-by: Jonathan <jonathancw@me.com>

--- source/Functions/program_settings/program_settings.cpp
+++ source/Functions/program_settings/program_settings.cpp
@@ -1,6 +1,11 @@
-
+#include <string>
+#include <vector>
+#include <regex>
+#include <iostream>
 
 #include "program_settings.hpp"
+#include "global_defines.hpp"
+#include "common.hpp"
 
 namespace
 {
@@ -9,4 +14,55 @@ namespace
 /** regex_data member functions: */
 namespace settings
 {
+    regex_data& regex_data::operator=(const regex_data& reg) noexcept
+    {
+        if(this != &reg)
+        {
+            this->on = reg.on;
+            this->s = reg.s;
+        }
+        return *this;
+    }
+    
+    bool regex_data::operator==(const regex_data& r) const noexcept
+    {
+        return (
+                    (this->s == r.s) && 
+                    (this->on == r.on));
+    }
+    
+    bool regex_data::operator!=(const regex_data& r) const noexcept
+    {
+        return !(this->operator==(r));
+    }
+    
+    std::ostream& operator<<(std::ostream& out, const regex_data& r)
+    {
+        if(out.good())
+        {
+            out<< r.s<< mem_delim::value;
+            out<< r.on<< mem_delim::value;
+        }
+        return out;
+    }
+    
+    std::istream& operator>>(std::istream& in, regex_data& r)
+    {
+        r = regex_data();
+        
+        in.peek();
+        if(in.good())
+        {
+            if(common::safe_getline(in, r.s, mem_delim::value))
+            {
+                bool tempb(false);
+                common::safe_getline(in, tempb, mem_delim::value);
+                r.on = tempb;
+            }
+        }
+        in.peek();
+        return in;
+    }
+    
+    
 }
--- source/Functions/program_settings/program_settings.hpp
+++ source/Functions/program_settings/program_settings.hpp
@@ -2,37 +2,49 @@
 #define PROGRAM_SETTINGS_HPP_INCLUDED
 #include <regex>
 #include <vector>
+#include <iostream>
 
 namespace settings
 {
-    struct settings_data;
-    struct regex_data;
-    struct regex_settings_data;
+    typedef struct settings_data settings_data;
+    typedef struct regex_data regex_data;
+    typedef struct regex_settings_data regex_settings_data;
     
+    
+    std::istream& operator>>(std::istream&, regex_data&);
+    std::ostream& operator<<(std::ostream&, const regex_data&);
     
     /* One Regular expression. */
-    struct regex_data
+    typedef struct regex_data
     {
+        explicit regex_data() noexcept
+        {
+        }
+        
+        ~regex_data() noexcept
+        {
+        }
+        
         /* cur_pos write operators for each settings structure.  We'll also 
          * need stream operators for saving them. */
-        regex_data& operator=(const regex_data&);
-        bool operator==(const regex_data&) const;
-        bool operator!=(const regex_data&) const;
+        regex_data& operator=(const regex_data&) noexcept;
+        bool operator==(const regex_data&) const noexcept;
+        bool operator!=(const regex_data&) const noexcept;
         
         bool on : 1;
         std::string s;
-    };
+    } regex_data;
     
-    struct regex_settings_data
+    typedef struct regex_settings_data
     {
         bool use_regex, use_match, use_not_match;
         std::vector<regex_data> match, not_match;
-    };
+    } regex_settings_data;
     
-    struct settings_data
+    typedef struct settings_data
     {
         regex_settings_data regex_settings;
-    };
+    } settings_data;
     
     
 }
